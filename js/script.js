b868cfe38e43e625a36c53dffc8b296bf64455d6
Dillan Arzate <dillan.arzate@gmail.com>
2019-04-23 14:59:06 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Here the global varibles are defined.
+//Here the global varibles are defined to be used in the functions.
 const mainDiv = document.querySelector('.page');
 const students = document.getElementsByTagName('li');
 
@@ -16,21 +16,29 @@ const students = document.getElementsByTagName('li');
        that will be passed into the parens later when you call or
        "invoke" the function
 ***/
+
+/***
+    -Here a function is created that takes in the parameters of the
+     list of students and the number page that they will be on.
+    -A for loop is created so that each individual list item is iterated
+     through to determine which page they will be displayed on.
+***/
 const showPage = (list, page) => {
       const pageItems = 10;
+      const listLength = list.length;
       let startList = (page * pageItems) - pageItems;
       let endList = page * pageItems;
 
-      for (let i = 0; i <= list.length; i++) {
-          if (list[i] >= startList && list[i] <= endList) {
+      for (let i = 0; i < listLength; i++)
+          if (list[i] >= startList && list[i] < endList) {
             list[i].style.display = 'block';
           } else {
               list[i].style.display = 'none';
           }
 
-      }
+      };
 
-};
+
 
 
 
@@ -49,8 +57,3 @@ const appendPageLinks = (list) => {
 
       }
 };
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
