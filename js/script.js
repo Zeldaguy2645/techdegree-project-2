a66895bb2a89e249b9bb6b1071dace0f8ba0fd26
Dillan Arzate <dillan.arzate@gmail.com>
2019-05-14 14:27:00 -0500

    Update script.js
    In final stages of the project.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 //Here the global varibles are defined to be used in the functions.
-const mainDiv = document.querySelector('.page');
 const students = document.getElementsByTagName('li');
-let div = document.createElement('div');
-let ul = document.createElement('ul');
 
 /***
     -Here a function is created that takes in the parameters of the
@@ -31,25 +28,28 @@ const showPage = (list, page) => {
     correct page number and correct number of students.
 ***/
 const appendPageLinks = (list) => {
-      let li = document.createElement('li');
-      let a = document.createElement('a');
-      let pages = Math.ceil(list.length / 10);
-      div.className = 'pagination';
-      mainDiv.appendChild(div);
-      div.appendChild(ul);
+      const pages = Math.ceil(list.length / 10);
 
 
-      for (let i = 0; i <= pages; i++) {
+
+      for (let i = 1; i <= pages; i++) {
+          let mainDiv = document.querySelector('.page');
+          let li = document.createElement('li');
+          let a = document.createElement('a');
+          let div = document.createElement('div');
+          let ul = document.createElement('ul');
+          div.className = 'pagination';
+          mainDiv.appendChild(div);
+          div.appendChild(ul);
           ul.appendChild(li);
           li.appendChild(a);
-          a.textContent.value = pages[i];
-        }
+          a.textContent = i;
 
-      for () {
-        a.addEventListener('click', (e) => {
-          a.target.event = showPage(students, 4);
-        })
-      }
+          a.addEventListener('click', (e) => {
+            a.target.event = showPage(students, i);
+            a.className = "active";
+          })
+        }
 };
 showPage(students, 1);
 appendPageLinks(students);
